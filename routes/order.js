f385414022efd43636bd75f54f814c20eb843004
quocthai <lxlthailxl@gmail.com>
2025-02-19 20:54:51 +0700

    add routers for revenue analysis

--- routes/order.js
+++ routes/order.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { callBackVnPay, getOrders, exportOrdersToExcelFile, countOrders, totalRevenue, countOrdersByCinema } = require('../controllers/orderController');
+const { callBackVnPay, getOrders, exportOrdersToExcelFile, countOrders, totalRevenue, countOrdersByCinema, getTotalRevenueByCinema, getTotalRevenueByMonth, getTotalRevenueByMovie } = require('../controllers/orderController');
 
 const router = express.Router()
 
@@ -11,5 +11,8 @@ router.get('/order/export', protect, authorize('admin'), exportOrdersToExcelFile
 router.get('/order/total', protect, authorize('admin'), countOrders);
 router.get('/order/revenue', protect, authorize('admin'), totalRevenue);
 router.get('/order/analysis', protect, authorize('admin'), countOrdersByCinema);
+router.get('/order/revenue-by-cinema', protect, authorize('admin'), getTotalRevenueByCinema);
+router.get('/order/revenue-by-movie', protect, authorize('admin'), getTotalRevenueByMovie);
+router.get('/order/revenue-by-month', protect, authorize('admin'), getTotalRevenueByMonth);
 
 module.exports = router
