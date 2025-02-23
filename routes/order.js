7d15bc3e5aefefab0a2964a19b113c1b37d675aa
NguyenThiNga111 <nguyentronghuu.07052003@gmail.com>
2025-02-23 23:20:44 +0700

    update code

--- routes/order.js
+++ routes/order.js
@@ -1,18 +1,47 @@
-const express = require('express');
-const { callBackVnPay, getOrders, exportOrdersToExcelFile, countOrders, totalRevenue, countOrdersByCinema, getTotalRevenueByCinema, getTotalRevenueByMonth, getTotalRevenueByMovie } = require('../controllers/orderController');
+const express = require("express");
+const {
+  callBackVnPay,
+  getOrders,
+  exportOrdersToExcelFile,
+  countOrders,
+  totalRevenue,
+  countOrdersByCinema,
+  getTotalRevenueByCinema,
+  getTotalRevenueByMonth,
+  getTotalRevenueByMovie,
+} = require("../controllers/orderController");
 
-const router = express.Router()
+const router = express.Router();
 
-const { protect, authorize } = require('../middleware/auth');
+const { protect, authorize } = require("../middleware/auth");
 
-// router.get('/call-back/vnpay', callBackVnPay);
-router.get('/admin/orders', protect, authorize('admin'), getOrders);
-router.get('/order/export', protect, authorize('admin'), exportOrdersToExcelFile);
-router.get('/order/total', protect, authorize('admin'), countOrders);
-router.get('/order/revenue', protect, authorize('admin'), totalRevenue);
-router.get('/order/analysis', protect, authorize('admin'), countOrdersByCinema);
-router.get('/order/revenue-by-cinema', protect, authorize('admin'), getTotalRevenueByCinema);
-router.get('/order/revenue-by-movie', protect, authorize('admin'), getTotalRevenueByMovie);
-router.get('/order/revenue-by-month', protect, authorize('admin'), getTotalRevenueByMonth);
+router.get("/admin/orders", protect, authorize("admin"), getOrders);
+router.get(
+  "/order/export",
+  protect,
+  authorize("admin"),
+  exportOrdersToExcelFile
+);
+router.get("/order/total", protect, authorize("admin"), countOrders);
+router.get("/order/revenue", protect, authorize("admin"), totalRevenue);
+router.get("/order/analysis", protect, authorize("admin"), countOrdersByCinema);
+router.get(
+  "/order/revenue-by-cinema",
+  protect,
+  authorize("admin"),
+  getTotalRevenueByCinema
+);
+router.get(
+  "/order/revenue-by-movie",
+  protect,
+  authorize("admin"),
+  getTotalRevenueByMovie
+);
+router.get(
+  "/order/revenue-by-month",
+  protect,
+  authorize("admin"),
+  getTotalRevenueByMonth
+);
 
-module.exports = router
+module.exports = router;
