43da0de48957704a441fd4ff3073cbdd1c81ae2e
PhanHoangPhuc30 <Php3002@gmail.com>
2025-02-23 23:23:04 +0700

    update1

--- models/seatAvailable.js
+++ models/seatAvailable.js
@@ -1,21 +1,21 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const seatAvailableSchema = new mongoose.Schema(
-  {
-    movieShowingId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "MovieShowing",
-    },
-    seatId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Seat",
-    },
-    isAvailable: {
-      type: Boolean,
-      default: true,
-    },
-  },
-  { timestamps: true }
-);
+// const seatAvailableSchema = new mongoose.Schema(
+//   {
+//     movieShowingId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "MovieShowing",
+//     },
+//     seatId: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Seat",
+//     },
+//     isAvailable: {
+//       type: Boolean,
+//       default: true,
+//     },
+//   },
+//   { timestamps: true }
+// );
 
-module.exports = mongoose.model("SeatAvailable", seatAvailableSchema);
+// module.exports = mongoose.model("SeatAvailable", seatAvailableSchema);
