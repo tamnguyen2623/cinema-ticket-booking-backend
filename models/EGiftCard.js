4b0ba22f08f0541e39cf84720c3844bdbba931a6
Huynhkiet2510 <belikeme409@gmail.com>
2025-03-10 16:08:41 +0700

    update egift

--- models/EGiftCard.js
+++ models/EGiftCard.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const EGiftSchema = new mongoose.Schema(
   {
-    giftNumber: {
-      type: Date,
+    name: {
+      type: String,
     },
-    expirationDate: {
-      type: Number,
+    image: {
+      type: String,
     },
-    balance: {
-      type: Number,
+    desription: {
+      type: String,
     },
     isDelete: {
       type: Boolean,
